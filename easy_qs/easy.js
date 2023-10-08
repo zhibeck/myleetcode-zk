8262d6a59c284306631cd50a071f58223fc6e1b4
zhibeck <113952797+zhibeck@users.noreply.github.com>
2023-10-08 16:34:55 -0700

    added new solutions

--- easy_qs/easy.js
+++ easy_qs/easy.js
@@ -243,3 +243,17 @@ var isEmpty = function (obj) {
     return false;
   }
 };
+
+//
+
+var removeElement = function (nums, val) {
+  let i = 0;
+
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+      nums[i] = nums[j];
+      i++;
+    }
+  }
+  return i;
+};
